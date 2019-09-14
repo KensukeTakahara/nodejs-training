db37dd34266cfbeaf8bc7ef8da9b60e8758e1a08
Kensuke Takahara <zawcjk@gmail.com>
2019-09-14 23:17:45 +0900

    implement read file function.

--- primir/node-app/app.js
+++ primir/node-app/app.js
@@ -1,15 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
-let server = http.createServer((request, response) => {
-  response.setHeader("Content-Type", "text/html");
-  response.write('<!DOCTYPE html><html lang="ja">');
-  response.write('<head><meta charset="utf-8">');
-  response.write("<title>Hello</title></head>");
-  response.write("<body><h1>Hello Node.js!</h1>");
-  response.write("<p>This is Node.js sample page.</p>");
-  response.write("<p>これは、Node.jsのサンプルページです。</p>", "utf8");
-  response.write("</body></html>");
-  response.end();
-});
+const getFromClient = (req, res) => {
+  const data = fs.readFileSync("./index.html", "UTF-8");
+  writeToResponse(res, data);
+};
+
+const writeToResponse = (res, data) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(data);
+  res.end();
+};
+
+let server = http.createServer(getFromClient);
 server.listen(3000);
 console.log("Server start!");
