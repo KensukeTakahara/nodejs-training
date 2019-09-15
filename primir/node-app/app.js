02e2117b32fe0ad4406aa3c0b8bfa1e0960c64bb
Kensuke Takahara <zawcjk@gmail.com>
2019-09-15 12:08:10 +0900

    finished template engine.

--- primir/node-app/app.js
+++ primir/node-app/app.js
@@ -1,21 +1,22 @@
 const http = require("http");
 const fs = require("fs");
+const ejs = require("ejs");
+
+const index_page = fs.readFileSync("./index.ejs", "utf8");
 
 const getFromClient = (req, res) => {
-  const data = fs.readFileSync("./index.html", "UTF-8");
+  const data = ejs.render(index_page, {
+    title: "Index",
+    content: "これはテンプレートを使ったサンプルページです。"
+  });
   writeToResponse(res, data);
 };
 
 const writeToResponse = (res, data) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(replaceHtmlContent(data));
+  res.write(data);
   res.end();
 };
-
-const replaceHtmlContent = data =>
-  data
-    .replace("dummy_title", "タイトルです。")
-    .replace("dummy_content", "これがコンテンツです。");
 
 let server = http.createServer(getFromClient);
 server.listen(3000);
