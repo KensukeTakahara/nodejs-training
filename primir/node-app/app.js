d102ff17869b6a6a41293e17d9860c158a887db5
Kensuke Takahara <zawcjk@gmail.com>
2019-09-17 02:00:30 +0900

    implemented form.

--- primir/node-app/app.js
+++ primir/node-app/app.js
@@ -2,20 +2,17 @@ const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
 const url = require("url");
+const qs = require("querystring");
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
 const other_page = fs.readFileSync("./other.ejs", "utf8");
 const style_css = fs.readFileSync("./style.css", "utf8");
 
 const getFromClient = (req, res) => {
-  const url_place = url.parse(req.url);
+  const url_place = url.parse(req.url, true);
   switch (url_place.pathname) {
     case "/": {
-      const data = ejs.render(index_page, {
-        title: "Index",
-        content: "これはテンプレートを使ったサンプルページです。"
-      });
-      writeToResponse(res, data, "text/html");
+      responseIndex(req, res);
       break;
     }
     case "/style.css": {
@@ -23,17 +20,48 @@ const getFromClient = (req, res) => {
       break;
     }
     case "/other": {
-      const data = ejs.render(other_page, {
-        title: "Other",
-        content: "これは新しく用意したページです。"
-      });
-      writeToResponse(res, data, "text/html");
+      responseOther(req, res);
       break;
     }
     default: {
       writeToResponse(res, "no page...", "text/plain");
       break;
     }
+  }
+};
+
+const responseIndex = (request, response) => {
+  let content = "これはテンプレートを使ったサンプルページです。";
+  const data = ejs.render(index_page, {
+    title: "Index",
+    content
+  });
+  writeToResponse(response, data, "text/html");
+};
+
+const responseOther = (request, response) => {
+  let msg = "これはOtherページです。";
+  if (request.method == "POST") {
+    let body = "";
+    request.on("data", data => {
+      body += data;
+    });
+    request.on("end", () => {
+      let post_data = qs.parse(body);
+      msg += "あなたは、「" + post_data.msg + "」と書きました。";
+      const data = ejs.render(other_page, {
+        title: "Other",
+        content: msg
+      });
+      writeToResponse(response, data, "text/html");
+    });
+  } else {
+    msg += "ページがありません。";
+    const data = ejs.render(other_page, {
+      title: "Other",
+      content: msg
+    });
+    writeToResponse(response, data, "text/html");
   }
 };
 
