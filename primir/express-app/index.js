882934411e9ce05324ede241bbf41a51eaa2f99c
Kensuke Takahara <zawcjk@gmail.com>
2019-09-20 00:08:22 +0900

    implement table.

--- primir/express-app/index.js
+++ primir/express-app/index.js
@@ -8,21 +8,29 @@ app.use(express.static("public"));
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const data = {
+  Taro: "taro@yamada",
+  Hanako: "hanako@flower",
+  Sachiko: "sachi@happy",
+  Ichiro: "ichi@baseball"
+};
+
 app.get("/", (req, res) => {
-  const msg = "This is Index Page!<br />※メッセージを書いて送信してください。";
+  const msg = "This is Index Page!<br />※データを表示します。";
   res.render("index.ejs", {
     title: "Index",
-    content: msg
+    content: msg,
+    data: data
   });
 });
 
-app.post("/", (req, res) => {
-  const msg = `This is Posted Page!<br />あなたは「${req.body.message}」と送信しました。`;
-  res.render("index.ejs", {
-    title: "Posted",
-    content: msg
-  });
-});
+// app.post("/", (req, res) => {
+//   const msg = `This is Posted Page!<br />あなたは「${req.body.message}」と送信しました。`;
+//   res.render("index.ejs", {
+//     title: "Posted",
+//     content: msg
+//   });
+// });
 
 app.listen(3000, () => {
   console.log("Start server port:3000");
