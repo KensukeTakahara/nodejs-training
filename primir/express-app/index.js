cc22e802f657b244acda1c46db928161f9bfa9f5
Kensuke Takahara <zawcjk@gmail.com>
2019-09-19 23:40:46 +0900

    applied style.

--- primir/express-app/index.js
+++ primir/express-app/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const ejs = require("ejs");
+
 const app = express();
 
+app.engine("ejs", ejs.renderFile);
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Welcome to Express!");
+  const msg =
+    "This is Express Page!<br />これは、スタイルシートを利用した例です。";
+  res.render("index.ejs", {
+    title: "Index",
+    content: msg
+  });
 });
 
 app.listen(3000, () => {
