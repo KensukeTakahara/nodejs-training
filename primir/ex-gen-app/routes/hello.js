5de057f6a53456ba1cbc14807e263c7954bb65b9
Kensuke Takahara <zawcjk@gmail.com>
2019-09-21 13:39:54 +0900

    practice query.

--- primir/ex-gen-app/routes/hello.js
+++ primir/ex-gen-app/routes/hello.js
@@ -2,9 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res, next) => {
+  const name = req.query.name;
+  const mail = req.query.mail;
   var data = {
     title: "Hello",
-    content: "これは、サンプルのコンテンツです。<br />this is sample page."
+    content: `あなたの名前は${name}です。<br />メールアドレスは${mail}です。`
   };
   res.render("hello", data);
 });
