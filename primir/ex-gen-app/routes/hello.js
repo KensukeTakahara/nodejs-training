25b244602148eab9d7ff0d24b8fb57ce15c47f9c
Kensuke Takahara <zawcjk@gmail.com>
2019-09-21 13:44:56 +0900

    practice body parser

--- primir/ex-gen-app/routes/hello.js
+++ primir/ex-gen-app/routes/hello.js
@@ -2,11 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res, next) => {
-  const name = req.query.name;
-  const mail = req.query.mail;
   var data = {
     title: "Hello",
-    content: `あなたの名前は${name}です。<br />メールアドレスは${mail}です。`
+    content: "何かを書いて送信してください。"
+  };
+  res.render("hello", data);
+});
+
+router.post("/post", (req, res, next) => {
+  const msg = req.body["message"];
+  const data = {
+    title: "Hello",
+    content: `あなたは「${msg}」と送信しました。`
   };
   res.render("hello", data);
 });
