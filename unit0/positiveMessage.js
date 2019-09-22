10adf279a38541b2ba72673955a3dada52cf852f
Kensuke Takahara <zawcjk@gmail.com>
2019-09-23 01:17:54 +0900

    finished unit 0.

---
+++ unit0/positiveMessage.js
@@ -0,0 +1,8 @@
+"use strict";
+let message = [
+  "A change of environment can be a good thing!",
+  "You will make it!",
+  "Just run with the code."
+];
+
+message.forEach(word => console.log(word));
