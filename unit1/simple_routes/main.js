d61219ab611862c9b1543604f4ff5fe7837a22b3
Kensuke Takahara <zawcjk@gmail.com>
2019-09-25 23:23:48 +0900

    practice lesson5.

--- unit1/simple_routes/main.js
+++ unit1/simple_routes/main.js
@@ -2,7 +2,10 @@
 
 const routeResponseMap = {
   "/info": "<h1>Info Page</h1>",
-  "/contact": "<h1>Contact us</h1>"
+  "/contact": "<h1>Contact us</h1>",
+  "/about": "<h1>Learn More About Us.</h1>",
+  "/hello": "<a href='mailto:test@example'>here</a>",
+  "/error": "<p>Sorry, the page you are looking for is not here.</p>"
 };
 
 const port = 3000;
